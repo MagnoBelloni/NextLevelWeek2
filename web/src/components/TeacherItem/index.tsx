0e2349add4a40c12c498c98fa71eb37cf51ee20a
Magno Belloni <bellonidesouza6@gmail.com>
2020-08-05 16:30:02 -0300

    Finished Web

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,30 +3,48 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-const TeacherItem: React.FC = () => {
+export interface ITeacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    user_id: string;
+    whatsapp: string;
+}
+
+interface ITeacherItemProps {
+    teacher: ITeacher
+}
+
+const TeacherItem: React.FC<ITeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id
+        });
+    }
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://i.pinimg.com/originals/21/89/67/2189679cd7afafb0d1988a9d0baa88d2.jpg" alt="Solaire" />
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Solaire</strong>
-                    <span>Religião</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
             <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                <br />
-                <br />
-                        Temporibus ex, asperiores doloribus sed voluptatibus eius necessitatibus cum. Aperiam blanditiis beatae sit, asperiores eos iste ab vero, accusantium et, animi inventore.
+                {teacher.bio}
             </p>
             <footer>
-                <p>Preço/hora <strong>20R$</strong></p>
-                <button type="button">
+                <p>Preço/hora <strong>R${teacher.cost}</strong></p>
+                <a onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`} target="__blank" type="button">
                     <img src={whatsappIcon} alt="Whatsapp" />
                             Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     )
